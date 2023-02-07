3b314c4869b29ee4d0f15f68e037f737a4a8c1c0
Roman <rsamohwal@gmail.com>
2023-02-07 18:05:28 +0300

    Рефакторинг файлов

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -5,6 +5,7 @@ export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
+    src: string;
 }
 
 export interface BuildEnv {
@@ -18,3 +19,4 @@ export interface BuildOptions {
     isDev: boolean;
     port: number;
 }
+
