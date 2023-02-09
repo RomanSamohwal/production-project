2d8cd79dc2f8c1ee928d2fe461688404f7a65330
Roman <rsamohwal@gmail.com>
2023-02-09 16:19:45 +0300

    Добавил интернациализацию

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "app/providers/ThemeProvider";
 import ReactDOM from 'react-dom/client'
+import "./shared/config/i18n/i18n";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,4 +1,4 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import {classNames} from "shared/lib/classNames/classNames";
 import cls from './AppLink.module.scss';
 import {Link, LinkProps} from "react-router-dom";
 import {FC} from "react";
@@ -23,10 +23,11 @@ export const AppLink: FC<AppLinkProps> = (props) => {
         ...otherProps
     } = props;
 
+
     return (
         <Link
             to={to}
-            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            className={classNames(cls.AppLink, {[cls[theme]]: true}, [className])}
             {...otherProps}
         >
             {children}
