7b44e3b011ec7af08caed91cf9b93aae748fe174
Roman <rsamohwal@gmail.com>
2023-02-11 19:11:11 +0300

    Добавил eslint, stylelint

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,12 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
-
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
-        use: ['@svgr/webpack']
-    }
+        use: ['@svgr/webpack'],
+    };
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -21,36 +20,35 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
-                            ? "[path][name]__[local]--[hash:base64:5]"
-                            : "[hash:base64:8]"
-                    }
-                }
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
             },
             // Compiles Sass to CSS
             'sass-loader',
-        ]
-    }
+        ],
+    };
 
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-    }
+    };
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
             {
-                loader: 'file-loader'
-            }
-        ]
-    }
+                loader: 'file-loader',
+            },
+        ],
+    };
 
     return [
         fileLoader,
         svgLoader,
         typescriptLoader,
         cssLoader,
-    ]
+    ];
 }
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import App from "./app/App";
-import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "app/providers/ThemeProvider";
-import ReactDOM from 'react-dom/client'
-import "./shared/config/i18n/i18n";
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './shared/config/i18n/i18n';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <BrowserRouter>
         <ThemeProvider>
             <App />
         </ThemeProvider>
-    </BrowserRouter>
-)
+    </BrowserRouter>,
+);
