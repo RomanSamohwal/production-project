c6afda83c880a8de62f136b5f1a7a722560b2f13
Roman <rsamohwal@gmail.com>
2023-02-16 17:41:08 +0300

    Добавил ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'app/providers/ThemeProvider';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './shared/config/i18n/i18n';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
 );
