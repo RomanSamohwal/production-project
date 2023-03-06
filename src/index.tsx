22fc02ca1317a5e253c1a0bc19d2b5db830cc208
Roman <rsamohwal@gmail.com>
2023-03-06 10:18:52 +0300

    Добавил Modal

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
+import 'app/styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
