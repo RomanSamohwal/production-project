052332395cc1ad8dece8f70f535950d8d0a9236b
Roman <rsamohwal@gmail.com>
2023-04-13 12:21:58 +0300

    Рефактоиинг Sidebar

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -5,7 +5,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from '../../../../entities/User/model/selectors/getUserAuthData/getUserAuthData';
 import cls from './SidebarItem.module.scss';
-import { SidebarItemType } from '../../model/items';
+import { SidebarItemType } from 'widgets/Sidebar/model/types/sidebar';
 
 interface SidebarItemProps {
     item: SidebarItemType;
