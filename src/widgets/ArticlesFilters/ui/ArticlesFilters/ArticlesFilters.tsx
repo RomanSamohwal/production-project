edd2734c9084c6da94a54f3137257f83fd56dd8e
Roman <rsamohwal@gmail.com>
2023-09-19 22:18:06 +0300

    Redesign для ProfileCard.tsx

--- src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
+++ src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
@@ -8,7 +8,9 @@ import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import { ArticleSortField, ArticleType } from '@/entities/Article';
 import { SortOrder } from '@/shared/types/sort';
+import SearchIcon from '@/shared/assets/icons/search.svg';
 import { Input } from '@/shared/ui/redesigned/Input';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 
 interface ArticlesFiltersProps {
     className?: string;
@@ -46,6 +48,7 @@ export const ArticlesFilters = memo((props: ArticlesFiltersProps) => {
                     onChange={onChangeSearch}
                     value={search}
                     placeholder={t('Поиск')}
+                    addonLeft={<Icon Svg={SearchIcon} />}
                 />
                 <ArticleTypeTabs
                     value={type}
