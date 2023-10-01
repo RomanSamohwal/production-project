d22992f0425acf4ca38a88d83c2894c76b8cc389
Roman <rsamohwal@gmail.com>
2023-10-01 16:15:52 +0300

    Рефакторинг ListBox

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import ListIconDeprecated from '@/shared/assets/icons/list-24-24.svg';
 import TiledIconDeprecated from '@/shared/assets/icons/tiled-24-24.svg';
+
 import ListIcon from '@/shared/assets/icons/burger.svg';
 import TiledIcon from '@/shared/assets/icons/tile.svg';
 
@@ -13,9 +14,9 @@ import {
 import cls from './ArticleViewSelector.module.scss';
 import { ArticleView } from '@/entities/Article';
 import { ToggleFeatures, toggleFeatures } from '@/shared/lib/features';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 import { Card } from '@/shared/ui/redesigned/Card';
 import { HStack } from '@/shared/ui/redesigned/Stack';
-import { Icon } from '@/shared/ui/redesigned/Icon';
 
 interface ArticleViewSelectorProps {
     className?: string;
@@ -65,6 +66,7 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
                         {viewTypes.map((viewType) => (
                             <Icon
                                 clickable
+                                key={viewType.view}
                                 onClick={onClick(viewType.view)}
                                 Svg={viewType.icon}
                                 className={classNames('', {
